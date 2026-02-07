07361359c5321f0b2a35378f8d8e82702fcd0e4a
cody <musoudeus@gmail.com>
2026-02-07 21:00:25 +0800

    trying fix the router problem

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('api/comments/', views.comments_api),
-    path('api/comments/add/', views.comments_post_api),
+    path('', views.home, name='home'),              # ← 首頁
+    path('api/comments/', views.comments_api),      # ← GET
+    path('api/comments/add/', views.comments_post_api),  # ← POST
 ]
