8ae5c696046b3eb835b6a23aa81856e809b7e9d8
cody <musoudeus@gmail.com>
2026-02-07 20:15:00 +0800

    build mvc messege board

--- core/static/js/comments.js
+++ core/static/js/comments.js
@@ -1,14 +1,29 @@
 // addEvenetListener("DOMContentLoaded",...) 等待DOM建好
 document.addEventListener("DOMContentLoaded", () => {
   // 1. 在 DOM 裡面找到留言容器
-  const commentsBox = document.getElementById("comments");
+  // const commentsBox = document.getElementById("comments");
 
   // 2. 生出一塊 div  ( div 還在暫存器裡，未寫進 DOM )
-  const commentDiv = document.createElement("div");
+  // const commentDiv = document.createElement("div");
 
   // 3. 在這塊 div 內塞東西
-  commentDiv.innerText = "測試留言：Hello DOM";
+  // commentDiv.innerText = "測試留言：Hello DOM";
 
   // 4. 一次將處理好的 div 塞進 DOM
-  commentsBox.appendChild(commentDiv);
+  // commentsBox.appendChild(commentDiv);
+
+
+
+  fetch("/api/comments/")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("comment-list");
+
+    data.comments.forEach(item => {
+      const div = document.createElement("div");
+      div.innerHTML = `<strong>${item.author}</strong>: ${item.content}`;
+      container.appendChild(div);
+    });
+  });
+
 });
--- core/views.py
+++ core/views.py
@@ -1,4 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+def comments_api(request):
+    data = {
+        "comments":[
+            {"author": "Alice","content":"First Messege"},
+            {"author": "Bob","content":"Hello Django+Dom"},
+        ]
+    }
+
+    return JsonResponse(data)
