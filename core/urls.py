c4ab820f1d21dc45b84aa97c958de70b5bb7018e
cody <musoudeus@gmail.com>
2026-02-07 20:45:36 +0800

    modify the router(in urls.py)

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/comments/", views.comments_api),
+    path('', views.home, name='home'),
+    path('api/comments/', views.comments_api),
+    path('api/comments/add/', views.comments_post_api),
 ]
